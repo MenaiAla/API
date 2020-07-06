03a3b7765b27ac2d46d9b90d199b1f8f055e3c77
MenaiAla <ed.sheeran.eng@gmail.com>
2020-07-06 02:08:39 -0700

    :wrench: #777: replace 'null' value by null

--- utils/stringUtils.js
+++ utils/stringUtils.js
@@ -1,5 +1,5 @@
 
-const wordsStandardize = (word) => word ? word.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') : 'null';
+const wordsStandardize = (word) => word ? word.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') : null;
 
 
 const wordToBoolean = (word) => word ? ['true', '1'].includes(word.toString()) : false;
